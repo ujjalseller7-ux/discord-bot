4780f4f81a4332f66cd70066121d852c6ecc5520
ujjalseller7-ux <ujjalseller7@gmail.com>
2026-02-28 01:22:35 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, GatewayIntentBits, PermissionsBitField } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+const prefix = "!";
+const TOKEN = process.env.TOKEN;
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    message.reply("Pong!");
+  }
+});
+
+client.login(TOKEN);
